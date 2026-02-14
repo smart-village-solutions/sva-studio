d038b85d09e04abf2dfbaa3ba75fda5434433ee9
Philipp Wilimzig <p.wilimzig@ikusei.de>
2026-02-15 00:42:12 +0100

    refactor: remove deprecated core and routing exports from core package

--- packages/core/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const coreVersion = '0.0.1';
-export * from './routing/registry';
-//# sourceMappingURL=index.d.ts.map
--- packages/core/src/routing/registry.js
+++
@@ -1,5 +0,0 @@
-export const mergeRouteFactories = (core, plugins = []) => [...core, ...plugins];
-export const buildRouteTree = (rootRoute, factories) => {
-  const routes = factories.map((factory) => factory(rootRoute));
-  return rootRoute.addChildren(routes);
-};
